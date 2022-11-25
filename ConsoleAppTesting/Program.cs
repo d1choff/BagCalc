1df12b28727a78dc6c2164f8a40a98bcaf6e4bd6
Александр Козулин <a.cozulin@yandex.ru>
2022-11-25 15:38:35 +0900

    Добавьте файлы проекта.

---
+++ ConsoleAppTesting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Введите размерность массива:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            string[,] myArray = new string[x, y];
+            Console.Clear();
+            Console.WriteLine($"Размерность массива: {x},{y}");
+            Console.WriteLine();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    myArray[i, j] = Convert.ToChar(rnd.Next('a', 'Z' + 30)).ToString();
+                    Console.Write(myArray[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool swGoto = false;
+            double x, y, sum, enterSw;
+            do
+            {
+                Console.WriteLine("Выберите операцию:\n1 - Сложение, 2 - Вычитание, 3 - Умножение, 4 - Деление");
+                enterSw = Convert.ToInt32(Console.ReadLine());
+                switch (enterSw)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Вы выбрали сложение");
+                        Console.WriteLine("Введите первое число:");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        y = Convert.ToDouble(Console.ReadLine());
+                        sum = x - y;
+                        Console.Clear();
+                        Console.WriteLine($"Результат операции:\n{x} + {y} = {sum}\nНажмите любую клавишу, что бы вернуться на главный экран.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Вы выбрали вычитание");
+                        Console.WriteLine("Введите первое число:");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+                        sum = x + y;
+                        Console.WriteLine($"Результат операции:\n{x} - {y} = {sum}\nНажмите любую клавишу, что бы вернуться на главный экран.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Вы выбрали умножение");
+                        Console.WriteLine("Введите первое число:");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+                        sum = x * y;
+                        Console.WriteLine($"Результат операции:\n{x} * {y} = {sum}\nНажмите любую клавишу, что бы вернуться на главный экран.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Вы выбрали деление");
+                        Console.WriteLine("Введите первое число:");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите второе число:");
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+                        sum = x / y;
+                        Console.WriteLine($"Результат операции:\n{x} / {y} = {sum}\nНажмите любую клавишу, что бы вернуться на главный экран.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Такой операции не существует!");
+                        break;
+                }
+            }
+            while (swGoto == false);
+
+        }
+    }
+}
